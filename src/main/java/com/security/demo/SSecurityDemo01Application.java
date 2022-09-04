54d6e87cd75f6ad4543bbc78c8a6051467cb0cc2
Aditya Sharma <golu.sharma.dimapur@gmail.com>
2022-09-04 09:11:26 +0530

    first commit

---
+++ src/main/java/com/security/demo/SSecurityDemo01Application.java
@@ -0,0 +1,13 @@
+package com.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSecurityDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSecurityDemo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/demo/SSecurityDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSecurityDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
